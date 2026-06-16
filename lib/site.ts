5686bdf2e4c4be8bd5fe383582505547d08c4acb
gorkabarranquesa <gorkabarranquesa@gmail.com>
2026-06-16 15:38:20 +0200

    Add site data

---
+++ lib/site.ts
@@ -0,0 +1,9 @@
+export const site = {
+  name: "Kale Txiki Taberna",
+  shortName: "Kale Txiki",
+  tagline: "Taberna restaurante en Lakuntza con menú, barra, vermut, raciones, pizzas y hamburguesas caseras.",
+  address: "Mikel Arregi Kalea, 3, 31830 Lakuntza, Navarra",
+  phone: "948 57 60 09",
+  phoneHref: "tel:+34948576009",
+  mapsUrl: "https://www.google.com/maps/search/?api=1&query=Mikel%20Arregi%20Kalea%203%20Lakuntza%20Navarra",
+};

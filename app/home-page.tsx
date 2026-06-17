493f2a598c272132c470ab234f9a9c34cff29b9a
gorkabarranquesa <gorkabarranquesa@gmail.com>
2026-06-17 12:48:01 +0200

    Remove old homepage wrapper

--- app/home-page.tsx
+++
@@ -1,6 +0,0 @@
-import BusinessJsonLd from "./business-json-ld";
-import HomeContentV7 from "./home-content-v7";
-
-export default function HomePage() {
-  return <><BusinessJsonLd /><HomeContentV7 /></>;
-}

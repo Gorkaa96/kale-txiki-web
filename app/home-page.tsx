4133efdb93ca1eaa4232cd45667f5a48af83d73d
gorkabarranquesa <gorkabarranquesa@gmail.com>
2026-06-17 12:25:00 +0200

    Add homepage wrapper with structured data

---
+++ app/home-page.tsx
@@ -0,0 +1,6 @@
+import BusinessJsonLd from "./business-json-ld";
+import HomeContentV6 from "./home-content-v6";
+
+export default function HomePage() {
+  return <><BusinessJsonLd /><HomeContentV6 /></>;
+}
